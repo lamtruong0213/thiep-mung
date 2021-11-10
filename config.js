41faf6183d0097993deb972891eb585d5802d352
BELIAL <74252771+lamtruong0213@users.noreply.github.com>
2021-11-10 22:46:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng sinh nhật';
-const titleCard = 'Tặng tình yêu của anh';
+const titleCard = 'Gửi tình yêu của anh';
 const contentCard = 'Chúc honey của anh ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 const giftImage = 'hot-girl.png';
 const base64 = '';
